b9f4263046edb94154b362d56dd11dd6b6a24c53
Miguel R. Ávika <miguelravila@outlook.com>
2020-10-31 11:19:46 -0600

    Added sandbox

---
+++ Phase1/Mig3ru/h.cpp
@@ -0,0 +1,22 @@
+// Our Sandbox file 
+#include <iostream>
+#include <stack>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    int test = 0;
+    cin >> test;
+
+    for (size_t i = 0; i < test; i++)
+    {
+         cout << "ICPC" << endl;   
+    }
+
+
+    return 0;
+}
---
+++ Phase1/Mig3ru/sandbox.cpp
@@ -0,0 +1,17 @@
+// Our Sandbox file 
+#include <iostream>
+#include <stack>
+#include <deque>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+
+    cout << "ICPC" << endl;
+
+
+    return 0;
+}
